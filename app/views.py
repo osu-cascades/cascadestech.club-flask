d721b6cd38555a4fd978168d9d3cb742ea2e4a86
JWTappert <tappertj@oregonstate.edu>
2016-12-17 18:29:39 -0800

    Working on login form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-
+from .forms import LoginForm
 
 @app.route('/')
-def home(name=None):
-	return render_template('index.html', name=name)
+def home():
+	return render_template('index.html',
+							home=True,
+							heroText=True)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+				(form.username.data, str(form.remember_me.data)))
+		return redirect('/')
+	return render_template('login.html',
+							home=False,
+							heroText=False,
+							form=form)
