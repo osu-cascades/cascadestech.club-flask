9b5e7e2f737575bfc0704b1af2d77ec80952c326
JWTappert <tappertj@oregonstate.edu>
2016-12-20 20:33:43 -0800

    Removed dummy email credentials

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class DevelopmentConfig(Config):
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'donotreply.ctc@gmail.com'
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'CascadesTechClub'
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
