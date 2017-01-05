9421847dcffb18c86ce6f7e9ca0274d7065f27a9
JWTappert <tappertj@oregonstate.edu>
2017-01-05 14:26:37 -0800

    Working towards deployment

--- config.py
+++ config.py
@@ -37,28 +37,43 @@ class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
-    @classmethod
-    def init_app(cls, app):
-    	Config.init_app(app)
+    	@classmethod
+    	def init_app(cls, app):
+    		Config.init_app(app)
 
-    	# email errors to the administrators
-    	import logging
-    	from logging_handlers import SMTPHandler
-    	credentials = None
-    	secure = None
-    	if getattr(cls, 'MAIL_USERNAME', None) is not None:
-    		credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-    		if getattr(cls, 'MAIL_USE_TLS', None):
-    			secure()
-    	mail_handler = SMTPHandler(
-    		mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-    		fromaddr=cls.CTC_MAIL_SENDER,
-    		toaddrs=[cls.CTC_ADMIN],
-    		subject=cls.CTC_MAIL_SUBJECT_PREFIX + 'Application Error',
-    		credentials=credentials,
-    		secure=secure)
-    	mail_handler.setLevel(logging.ERROR)
-    	app.logger.addHandler(mail_handler)
+    		# email errors to the administrators
+    		import logging
+    		from logging_handlers import SMTPHandler
+    		credentials = None
+    		secure = None
+    		if getattr(cls, 'MAIL_USERNAME', None) is not None:
+    			credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+    			if getattr(cls, 'MAIL_USE_TLS', None):
+    				secure()
+    				mail_handler = SMTPHandler(
+    					mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+    					fromaddr=cls.CTC_MAIL_SENDER,
+    					toaddrs=[cls.CTC_ADMIN],
+    					subject=cls.CTC_MAIL_SUBJECT_PREFIX + 'Application Error',
+    					credentials=credentials,
+    					secure=secure)
+    				mail_handler.setLevel(logging.ERROR)
+    				app.logger.addHandler(mail_handler)
+
+
+class HerokuConfig(ProductionConfig):
+	SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+	@classmethod
+	def init_app(cls, app):
+		ProductionConfig.init_app(app)
+
+		# log to stderr
+		import logging
+		from logging import StreamHandler
+		file_handler = StreamHandler()
+		file_handler.setLevel(logging.WARNING)
+		app.logger.addHandler(file_handler)
 
 
 config = {
